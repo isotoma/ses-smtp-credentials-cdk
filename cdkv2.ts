b5b25005e4d4ea95a1b605e2dd52a036c0e21f4d
Andrew Plummer <andrew.plummer@isotoma.com>
2025-07-02 10:58:32 +0100

    Upgrade Lambda runtime from 18 to 22 (#50)

--- cdkv2.ts
+++ cdkv2.ts
@@ -23,7 +23,7 @@ export class SesSmtpCredentialsProvider extends Construct {
         this.provider = new customResource.Provider(this, 'ses-smtp-credentials-provider', {
             onEventHandler: new lambda.Function(this, 'ses-smtp-credentials-event', {
                 code: lambda.Code.fromAsset(path.join(__dirname, 'provider')),
-                runtime: lambda.Runtime.NODEJS_18_X,
+                runtime: new lambda.Runtime('nodejs22.x', lambda.RuntimeFamily.NODEJS, { supportsInlineCode: true }),
                 handler: 'main.onEvent',
                 timeout: cdk.Duration.minutes(5),
                 initialPolicy: [
